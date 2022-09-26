671589a1864efdc1d5286a60b9ea7cb92dfed402
HwangYunu <lybear108@gmail.com>
2022-09-26 14:03:43 +0900

    List 컴포넌트 분리

--- src/components/Lists.js
+++ src/components/Lists.js
@@ -1,23 +1,8 @@
 import React from 'react';
 import { DragDropContext, Draggable, Droppable } from 'react-beautiful-dnd';
+import List from './List';
 
 export default function Lists({ todoData, setTodoData }) {
-  //삭제버튼
-  const handleClick = id => {
-    let newTodoData = todoData.filter(data => data.id !== id);
-    setTodoData(newTodoData);
-  };
-
-  const handleCompleteChange = id => {
-    let newTodoData = todoData.map(data => {
-      if (data.id === id) {
-        data.completed = !data.completed;
-      }
-      return data;
-    });
-    setTodoData(newTodoData);
-  };
-
   const handleEnd = result => {
     console.log(result);
     // 드래그시 result 매개변수에 source(기존위치), destination과 같은 드래그 이벤트 정보 출력
@@ -44,25 +29,15 @@ export default function Lists({ todoData, setTodoData }) {
               {todoData.map((data, index) => (
                 <Draggable key={data.id} draggableId={data.id.toString()} index={index}>
                   {(provided, snapshot) => (
-                    <div
-                      key={data.id}
-                      {...provided.draggableProps}
-                      ref={provided.innerRef}
-                      {...provided.dragHandleProps}
-                      className={`${
-                        snapshot.isDragging ? 'bg-gray-300' : 'bg-gray-100'
-                      } flex items-center justify-between w-full px-4 py-1 my-2 text-gray-600 border rounded`}
-                    >
-                      <div className="items-center">
-                        <input type="checkbox" defaultChecked={false} onChange={() => handleCompleteChange(data.id)} />
-                        <span className={data.completed ? 'line-through' : undefined}>{data.title}</span>
-                      </div>
-                      <div className="items-center">
-                        <button className="px-4 py-2" onClick={() => handleClick(data.id)}>
-                          x
-                        </button>
-                      </div>
-                    </div>
+                    <List
+                      id={data.id}
+                      title={data.title}
+                      completed={data.completed}
+                      todoData={todoData}
+                      setTodoData={setTodoData}
+                      provided={provided}
+                      snapshot={snapshot}
+                    />
                   )}
                 </Draggable>
               ))}
